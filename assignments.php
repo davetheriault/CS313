ede273f52f780a39e1efefa3f21321210dd85496
David Theriault <davetheriault@gmail.com>
2015-05-09 14:53:31 -0600

    css

---
+++ assignments.php
@@ -0,0 +1,38 @@
+<?php
+?>
+<html>
+    <head>
+        <title>CS313 Assignments</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="style.css">
+
+    </head>
+    <body>
+        <div id="container">
+            <div id="inner">
+        <div id="topnav">
+            <div id="maintitle">Assignments</div>
+            <ul id="topnavbar">
+                <li>Home</li>
+                <li>Assignments</li>
+                <li>Other</li>
+            </ul>
+        </div>
+        <div id="mainbody">
+            <img src="image/face.jpg" alt="Profile Picture" id="profile"/>
+            <div id="rightmain">
+            <div class="block">
+                <h2>Intro:</h2><br>
+                My name is David Theriault, and this is my project page for Web Engineering II.
+            </div>
+            <div class="block">
+                <h2>Assignments:</h2><br>
+            <a href="assignments.php">Assignments Page</a>
+            </div>
+            </div>
+        </div>
+        </div>
+        </div>
+    </body>
+</html>
