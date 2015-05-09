406316e83e96c71f03c58c4a0d993d372ecb51f2
David Theriault <davetheriault@gmail.com>
2015-05-09 10:54:48 -0600

    Commit 3

--- index.php
+++ index.php
@@ -7,6 +7,16 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
-        <div>Hello World!</div>
+        <div id="topnav">
+            <h1>Introduction</h1>
+        </div>
+        <div id="mainbody">
+            <img src="" alt="Profile Picture">
+            <h2>Intro:</h2>
+            <p>My name is David Theriault, and this is my project page for Web Engineering II.</p>
+            <h2>Assignments:</h2>
+            <p><a href="assignments.php">Assignments Page</a></p>
+            </ul>
+        </div>
     </body>
 </html>
