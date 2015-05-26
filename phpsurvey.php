a7e7ca37ae86a6dbef07bca842ff5ea2298ada0a
David Theriault <davetheriault@gmail.com>
2015-05-25 18:31:36 -0600

    Session

--- phpsurvey.php
+++ phpsurvey.php
@@ -1,4 +1,4 @@
-<?php if ($_SESSION['voted'] == 'voted') {header("Location: /results.php"); exit();} ?>
+<?php if ($_SESSION['voted'] == 'voted') {header("Location: http://php-dtheriault.rhcloud.com/results.php"); } ?>
 <html>
     <head>
         <title>CS313 Survey</title>
