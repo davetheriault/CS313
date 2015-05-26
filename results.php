a1e4727766ee34357c5bf78539ddc2c85fa271f0
David Theriault <davetheriault@gmail.com>
2015-05-25 20:31:54 -0600

    Results

--- results.php
+++ results.php
@@ -75,44 +75,44 @@ $tmn0Avg = round(($numTMNT0 / $movSum * 100), 2);
                 <?php include 'include/navbar.php'; ?>
 
         <div id="mainbody">
-            <h2><?php if (isset($_SESSION['voted'])){
-                echo 'Looks like you already completed the survey.';} ?></h2>
+            <?php if (isset($_SESSION['voted'])){
+                echo 'Your vote has been submitted. Thank you.';} ?>
             <h1>Results</h1>
             <div class="block">
                 <h2>Favorite Ninja Turtle:</h2><br><?php echo $resSum . ' total votes'; ?><br>
                 <ul class="barGraph">
-                    <?php echo "<li class=\"leobar\" style=\"width: " . $leoAvg*3 . "px;\">Leonardo<br>" . $leoAvg . "%</li>"; ?>
-                    <?php echo "<li class=\"donbar\" style=\"width: " . $donAvg*3 . "px;\">Donatello<br>" . $donAvg . "%</li>"; ?>
-                    <?php echo "<li class=\"rapbar\" style=\"width: " . $rapAvg*3 . "px;\">Raphael<br>" . $rapAvg . "%</li>"; ?>
-                    <?php echo "<li class=\"micbar\" style=\"width: " . $micAvg*3 . "px;\">Michaelangelo<br>" . $micAvg . "%</li>"; ?>
+                    <?php echo "<li class=\"leobar\" style=\"width: " . $leoAvg*5 . "px;\">Leonardo<br>" . $leoAvg . "%</li>"; ?>
+                    <?php echo "<li class=\"donbar\" style=\"width: " . $donAvg*5 . "px;\">Donatello<br>" . $donAvg . "%</li>"; ?>
+                    <?php echo "<li class=\"rapbar\" style=\"width: " . $rapAvg*5 . "px;\">Raphael<br>" . $rapAvg . "%</li>"; ?>
+                    <?php echo "<li class=\"micbar\" style=\"width: " . $micAvg*5 . "px;\">Michaelangelo<br>" . $micAvg . "%</li>"; ?>
                 </ul>
             </div>
             <div class="block">
                 <h2>Favorite Color:</h2><br><?php echo $colSum . ' total votes'; ?> <br>
                 <ul class="barGraph">
-                    <?php echo "<li class=\"leobar\" style=\"width: " . $bluAvg*3 . "px;\">Blue<br>" . $bluAvg . "%</li>"; ?>
-                    <?php echo "<li class=\"donbar\" style=\"width: " . $purAvg*3 . "px;\">Purple<br>" . $purAvg . "%</li>"; ?>
-                    <?php echo "<li class=\"rapbar\" style=\"width: " . $redAvg*3 . "px;\">Red<br>" . $redAvg . "%</li>"; ?>
-                    <?php echo "<li class=\"micbar\" style=\"width: " . $oraAvg*3 . "px;\">Orange<br>" . $oraAvg . "%</li>"; ?>
+                    <?php echo "<li class=\"leobar\" style=\"width: " . $bluAvg*5 . "px;\">Blue<br>" . $bluAvg . "%</li>"; ?>
+                    <?php echo "<li class=\"donbar\" style=\"width: " . $purAvg*5 . "px;\">Purple<br>" . $purAvg . "%</li>"; ?>
+                    <?php echo "<li class=\"rapbar\" style=\"width: " . $redAvg*5 . "px;\">Red<br>" . $redAvg . "%</li>"; ?>
+                    <?php echo "<li class=\"micbar\" style=\"width: " . $oraAvg*5 . "px;\">Orange<br>" . $oraAvg . "%</li>"; ?>
                 </ul>
             </div>
             <div class="block">
                 <h2>Favorite Villain:</h2><br><?php echo $vilSum . ' total votes'; ?> <br>
                 <ul class="barGraph">
-                    <?php echo "<li class=\"donbar\" style=\"width: " . $shrAvg*3 . "px;\">Shredder<br>" . $shrAvg . "%</li>"; ?>
-                    <?php echo "<li class=\"krabar\" style=\"width: " . $kraAvg*3 . "px;\">Krang<br>" . $kraAvg . "%</li>"; ?>
-                    <?php echo "<li class=\"rocbar\" style=\"width: " . $rocAvg*3 . "px;\">Rocksteady<br>" . $rocAvg . "%</li>"; ?>
-                    <?php echo "<li class=\"rapbar\" style=\"width: " . $beeAvg*3 . "px;\">Beebop<br>" . $beeAvg . "%</li>"; ?>
+                    <?php echo "<li class=\"donbar\" style=\"width: " . $shrAvg*5 . "px;\">Shredder<br>" . $shrAvg . "%</li>"; ?>
+                    <?php echo "<li class=\"krabar\" style=\"width: " . $kraAvg*5 . "px;\">Krang<br>" . $kraAvg . "%</li>"; ?>
+                    <?php echo "<li class=\"rocbar\" style=\"width: " . $rocAvg*5 . "px;\">Rocksteady<br>" . $rocAvg . "%</li>"; ?>
+                    <?php echo "<li class=\"rapbar\" style=\"width: " . $beeAvg*5 . "px;\">Beebop<br>" . $beeAvg . "%</li>"; ?>
                 </ul>
             </div>
             <div class="block">
                 <h2>Favorite Movie:</h2><br><?php echo $movSum . ' total votes'; ?> <br>
                 <ul class="barGraph">
-                    <?php echo "<li class=\"greenbar\" style=\"width: " . $tmn1Avg*3 . "px;\">Teenage_Mutant_Ninja_Turtles_(1990)<br>" . $tmn1Avg . "%</li>"; ?>
-                    <?php echo "<li class=\"greenbar\" style=\"width: " . $tmn2Avg*3 . "px;\">Teenage_Mutant_Ninja_Turtles_2:_The_Secret_of_the_Ooze<br>" . $tmn2Avg . "%</li>"; ?>
-                    <?php echo "<li class=\"greenbar\" style=\"width: " . $tmn3Avg*3 . "px;\">Teenage_Mutant_Ninja_Turtles_3<br>" . $tmn3Avg . "%</li>"; ?>
-                    <?php echo "<li class=\"greenbar\" style=\"width: " . $tmntAvg*3 . "px;\">TMNT<br>" . $tmntAvg . "%</li>"; ?>
-                    <?php echo "<li class=\"greenbar\" style=\"width: " . $tmn0Avg*3 . "px;\">Teenage_Mutant_Ninja_Turtles_(2014)<br>" . $tmn0Avg . "%</li>"; ?>
+                    <?php echo "<li class=\"greenbar\" style=\"width: " . $tmn1Avg*5 . "px;\">Teenage_Mutant_Ninja_Turtles_(1990)<br>" . $tmn1Avg . "%</li>"; ?>
+                    <?php echo "<li class=\"greenbar\" style=\"width: " . $tmn2Avg*5 . "px;\">Teenage_Mutant_Ninja_Turtles_2:_The_Secret_of_the_Ooze<br>" . $tmn2Avg . "%</li>"; ?>
+                    <?php echo "<li class=\"greenbar\" style=\"width: " . $tmn3Avg*5 . "px;\">Teenage_Mutant_Ninja_Turtles_3<br>" . $tmn3Avg . "%</li>"; ?>
+                    <?php echo "<li class=\"greenbar\" style=\"width: " . $tmntAvg*5 . "px;\">TMNT<br>" . $tmntAvg . "%</li>"; ?>
+                    <?php echo "<li class=\"greenbar\" style=\"width: " . $tmn0Avg*5 . "px;\">Teenage_Mutant_Ninja_Turtles_(2014)<br>" . $tmn0Avg . "%</li>"; ?>
                 </ul>
             </div>
         </div>
