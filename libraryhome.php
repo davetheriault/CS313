0b9b540cc4191ca9fea12a56fa72ed5ec5ffb4b4
David Theriault <davetheriault@gmail.com>
2015-05-27 22:46:56 -0600

    Library

--- libraryhome.php
+++ libraryhome.php
@@ -5,12 +5,13 @@
         <title>Mount Bruno Elementary Library</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="stylelib.css" type="text/css">
     </head>
     <body>
         <div id="container">
             <div id="inner">
                 <div id="topnavbar">
-                    <div id="logo"></div>
+                    <div id="maintitle">MOUNT-BRUNO</div>
                     <div id="topmenu">
                         <ul>
                             <li>Home</li>
