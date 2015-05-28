d5369dcfd7143013a8d68a4ddf50c7d910ae33e9
David Theriault <davetheriault@gmail.com>
2015-05-27 23:32:10 -0600

    Library style

--- libraryhome.php
+++ libraryhome.php
@@ -11,7 +11,7 @@
         <div id="container">
             <div id="inner">
                 <div id="topnav">
-                    <div id="maintitle">MOUNT-BRUNO<br>LIBRARY</div>
+                    <div id="maintitle">MOUNT-BRUNO LIBRARY</div>
                     
                     <ul id="topnavbar">
                         <li>Home</li>
