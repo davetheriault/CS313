5de50a6082f27b4894f7ee637cd81d573a52038e
David Theriault <davetheriault@gmail.com>
2015-06-18 15:42:27 -0600

    Library

--- assignments.php
+++ assignments.php
@@ -29,6 +29,9 @@
                 <h2><a href="scripture.php">Scripture Resources (TRA Lesson 04)</a></h2>
             </div>
             <div class="block">
+                <a href="libraryhome.php"></a>
+            </div>
+            <div class="block">
                 <a href="endsession.php">******</a>
             </div>
             
