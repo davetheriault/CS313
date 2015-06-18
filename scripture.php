8d4055f922f24d5d96d053d3f9e33cafec23edc5
David Theriault <davetheriault@gmail.com>
2015-06-18 12:02:04 -0600

    Scripture Thing

---
+++ scripture.php
@@ -0,0 +1,44 @@
+<?php
+require 'include/dbConnection.php';
+
+?>
+<html>
+    <head>
+        <title>Scripture Resources</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="style.css">
+
+    </head>
+    <body>
+        <div id="container">
+            <div id="inner">
+        
+                        <?php include 'include/navbar.php'; ?>
+
+        <div id="mainbody">
+            
+            <?php
+            foreach ($db->query('SELECT * FROM Scriptures') as $row){
+                echo '<div class="block"><strong>'. $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . '</strong> - \"'
+                        . $row['content'] . '\"</div>';
+            }
+            ?>
+            <div class="block">
+                <h2><a href="readiness.php">Team Readiness Activity</a></h2>
+            </div>
+            <div class="block">
+                <h2><a href="tmntsurvey.php">PHP Survey</a></h2>
+            </div>
+            <div class="block">
+                <h2><a href="libraryhome.php">PHP Library App</a></h2>
+            </div>
+            <div class="block">
+                <a href="endsession.php">******</a>
+            </div>
+            
+        </div>
+        </div>
+        </div>
+    </body>
+</html>
