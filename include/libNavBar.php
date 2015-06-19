09cc51044a5be35b3bd5a5b30ac245d8aa17d96d
David Theriault <davetheriault@gmail.com>
2015-06-19 12:05:17 -0600

    Library

---
+++ include/libNavBar.php
@@ -0,0 +1,12 @@
+<div id="topnav">
+    
+    <div id="maintitle">
+        MOUNT-BRUNO LIBRARY
+    </div>
+                    
+    <ul id="topnavbar">
+        <li>Home</li>
+        <li>Books</li>
+        <li>Login</li>
+    </ul>                    
+</div>
