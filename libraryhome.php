a6f27e17e175940fa6e05c6461a66e396c35b0bc
David Theriault <davetheriault@gmail.com>
2015-06-19 12:07:13 -0600

    Library

--- libraryhome.php
+++ libraryhome.php
@@ -21,7 +21,7 @@
                         <h3><a href="">Browse by Title</a></h3>
                     </div>
                     <div class="block">
-                        <h3><a href="">Browse by Author</a></h3>
+                        <h3><a href="librarybrowseauthor.php">Browse by Author</a></h3>
                     </div>
                 </div>
             </div>            
