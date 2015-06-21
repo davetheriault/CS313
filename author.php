0898950f8c75ff16349b64bf4cff36b1007bd25a
David Theriault <davetheriault@gmail.com>
2015-06-20 23:31:09 -0600

    Author Page

---
+++ author.php
@@ -0,0 +1,42 @@
+<?php
+require 'include/dbConnection.php';
+?>
+<html>
+    <head>
+        <title><?php echo $_GET; ?></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="stylelib.css" type="text/css">
+    </head>
+    <body>
+        <div id="container">
+            <div id="inner">
+                <?php 
+                include 'include/libNavBar.php';
+                ?>
+                <div id="mainbody">
+                    <h2>Books by <?php echo $_GET; ?></h2>
+                    <br><br>
+                    
+                    <?php 
+                        foreach ($db->query('SELECT DISTINCT author FROM book') as $row){
+                            if (TRUE){
+                            echo '<div class="block"><form action="author.php" method="get" id="author">'
+                                    . '<input class="submitbutton" type="submit" form="author" value="' . $row['author'] . '">'
+                                    . '</form></div>';
+                        }
+                        }
+                    ?>
+                    
+                    <div class="block">
+                        <h3><a href="">Browse by Title</a></h3>
+                    </div>
+                    <div class="block">
+                        <h3><a href="">Browse by Author</a></h3>
+                    </div>
+                </div>
+            </div>            
+        </div>
+    </body>
+</html>
+
